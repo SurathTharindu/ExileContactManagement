28e29ee086343e5998c73a93fa9171a69abe378f
Thilina Herath <thilinach87@gmail.com>
2013-03-25 11:20:54 +0530

    create NhibernateContext.cs

---
+++ ExileContactManagement.Tests/DBTest.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using ExileContactManagement.DBAccess;
+using NUnit.Framework;
+
+namespace ExileContactManagement.Tests
+{
+    [TestFixture]
+    public class DBTest
+    {
+        [Test]
+        public void RegisteredUserExistInDB()
+        {
+            var userMn=new UserManagement();
+
+        }
+    }
+}
---
+++ ExileContactManagement/DBAccess/NhibernateContext.cs
@@ -0,0 +1,43 @@
+﻿using ExileContactManagement.Models;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace ExileContactManagement.DBAccess
+{
+    public class NhibernateContext
+    {
+        private ISession _session;
+
+        public NhibernateContext()
+        {
+            _session = null;
+        }
+
+        public ISession Session
+        {
+            get
+            {
+                if (_session == null)
+                {
+                    var sessionFactory = CreateSessionFactory();
+                    var session = sessionFactory.OpenSession();
+                    _session = session;
+                }
+                return _session;
+            }
+        }
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+              .Database(MsSqlConfiguration.MsSql2008
+                            .ConnectionString(c => c
+                            .FromConnectionStringWithKey("Server=localhost; Port=3306; Database=ExileContactMgt; Uid=sa; Pwd=eXile123;")))
+              .Mappings(m => m.FluentMappings.AddFromAssemblyOf<User>())
+              .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(false, true))
+              .BuildSessionFactory();
+        }
+    }
+}
