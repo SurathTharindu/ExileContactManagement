27a5ab9a6bb31e8a8318247025c7269f7b90d648
SurathTharindu <suraththarindu@gmail.com>
2013-03-25 15:00:44 +0530

    untrack output files

---
+++ ExileContactManagement/Models/Search.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExileContactManagement.Models
+{
+    public class Search
+    {
+        [Required]
+        public String SearchString { get; set; }
+
+        public List<Contact> ResultList
+        {
+            get { return result; }
+            set { result = value; }
+        }
+        private List<Contact> result;
+
+        public Search()
+        {
+            SearchString = "";
+            result = new List<Contact>();
+        }
+    }
+}
