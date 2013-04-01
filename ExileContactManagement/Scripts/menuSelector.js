dd0cd17059ea046be586d00cc65261b14539fbf2
Thilina Herath <thilinach87@gmail.com>
2013-04-01 09:54:12 +0530

    Modify portal GUI

---
+++ ExileContactManagement/Scripts/menuSelector.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $("#menu li a").click(function (e) {
+        $("#menu li a").each(function () {
+            $(this).removeClass("active");
+        });
+
+        $(this).addClass("active");
+    });
+});
